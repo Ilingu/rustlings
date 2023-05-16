26aae53f7d9cd6474ec89ac941b66afed2c18753
Ilingu <ilingu@duck.com>
2023-05-16 12:32:21 +0200

    update

--- structs/structs1.rs
+++ structs/structs1.rs
@@ -2,8 +2,6 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 struct ColorClassicStruct {
     // TODO: Something goes here
     red: u8,
@@ -11,7 +9,7 @@ struct ColorClassicStruct {
     blue: u8,
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */u8,u8,u8);
+struct ColorTupleStruct(/* TODO: Something goes here */ u8, u8, u8);
 
 #[derive(Debug)]
 struct UnitLikeStruct;
@@ -23,7 +21,11 @@ mod tests {
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        let green = ColorClassicStruct {red:0,green:255,blue:0};
+        let green = ColorClassicStruct {
+            red: 0,
+            green: 255,
+            blue: 0,
+        };
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -33,7 +35,7 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        let green = ColorTupleStruct(0,255,0);
+        let green = ColorTupleStruct(0, 255, 0);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
